116c3b5d8a4364c68f6391f4eedb6d8fa87bdd06
noserronnie <93775605+noserronnie@users.noreply.github.com>
2023-03-14 14:07:27 +0100

    Finish Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import Link from '@mui/material/Link';
 import ProTip from './ProTip';
+import Navbar from './components/molecules/navbar';
 
 function Copyright() {
   return (
@@ -19,14 +20,17 @@ function Copyright() {
 
 export default function App() {
   return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Material UI Create React App example in TypeScript
-        </Typography>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <>
+      <Navbar />
+      <Container maxWidth="sm">
+        <Box sx={{ my: 4 }}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Material UI Create React App example in TypeScript
+          </Typography>
+          <ProTip />
+          <Copyright />
+        </Box>
+      </Container>
+    </>
   );
 }
